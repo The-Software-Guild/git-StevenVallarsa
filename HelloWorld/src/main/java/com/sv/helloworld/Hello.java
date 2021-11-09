26ba44abb9410f3c22eacb5497473578fb9d80e5
StevenVallarsa <orionsjv@gmail.com>
2021-11-08 23:32:28 -0500

    Hello World project

---
+++ HelloWorld/src/main/java/com/sv/helloworld/Hello.java
@@ -0,0 +1,11 @@
+package com.sv.helloworld;
+
+/**
+ *
+ * @author Steven Vallarsa
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }    
+}
